150edf92464093315bf8c9d0e38a7472122c8d23
Ritesh <ritesh.99981@gmail.com>
2019-11-13 15:16:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pushup1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.pushup1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //   Testing the Push Code feature of Git and Android Studio
+
+
+
+
+
+
+
+
+    }
+}
